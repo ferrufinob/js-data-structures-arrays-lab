c3f57fb3f8e5c92605129ec6f4a969cf9ce1bf6e
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-01 20:54:59 -0500

    commit tree 4867517b4144371fc2139dae19ee6bc18a810694

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  console.log(drivers.push(name));
+}
+// destructivelyAppendDriver("Brenda");
+
+function destructivelyPrependDriver(name) {
+  console.log(drivers.unshift(name));
+}
+// destructivelyPrependDriver("Carolina");
+
+function destructivelyRemoveLastDriver() {
+  console.log(drivers.pop());
+}
+// destructivelyRemoveLastDriver();
+
+function destructivelyRemoveFirstDriver() {
+  console.log(drivers.shift());
+}
+// destructivelyRemoveFirstDriver();
+
+function appendDriver(name) {
+  const newArr = [...drivers, name];
+  return newArr;
+}
+// console.log(appendDriver("Brenda"));
+
+function prependDriver(name) {
+  const newArr = [name, ...drivers];
+  return newArr;
+}
+// console.log(prependDriver("Brenda"));
+
+function removeLastDriver() {
+  const newArr = drivers.slice(0, -1);
+  return newArr;
+}
+// console.log(removeLastDriver());
+
+function removeFirstDriver() {
+  const newArr = drivers.slice(1);
+  return newArr;
+}
+// console.log(removeFirstDriver());
